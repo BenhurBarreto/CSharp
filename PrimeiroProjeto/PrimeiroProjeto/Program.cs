bda363c91db46f8f8284fb6f41354575b1e28d9b
Benhur <benhurbbs@gmail.com>
2019-06-19 11:30:52 -0300

    Inserindo variáveis de vários tipos

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -6,7 +6,18 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
+            SByte x = 100;
+            Byte n1 = 126;
+            int n2 = 1000, n3 = 2147483647;
+            long n4 = 2147483648L;
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine(x);
+            n1++;
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
         }
     }
 }
